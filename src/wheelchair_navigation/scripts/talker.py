c5765a865271841f85aeb7a863d14dd93e4b4386
ThaparPiyush <thaparpiyush123@gmail.com>
2022-09-06 16:12:27 +0530

    1. talker file added

---
+++ src/wheelchair_navigation/scripts/talker.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# license removed for brevity
+import rospy
+from std_msgs.msg import String
+
+def talker():
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        f = open("/home/rrc/commands.txt", "r")
+        hello_str = f.read() 
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
